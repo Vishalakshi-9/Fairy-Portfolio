a402c5bf7b94917af09078bed7531c8a8990788b
Vishalakshi-9 <vishalakshi927@gmail.com>
2025-07-22 01:06:01 +0530

    About.js

--- components/About.js
+++ components/About.js
@@ -20,7 +20,7 @@ export default function AboutSection() {
           Hi, I&apos;m <strong>Vishalakshi Kumari</strong>, a B.Tech student in Computer Science & Engineering at KIIT, with a passion for transforming code into beautiful, intuitive experiences. I'm an aspiring software developer who loves solving real-world problems through clean, efficient code.
         </p>
         <p className="text-lg text-yellow-100 font-light mt-4">
-          I enjoy building creative UIs, experimenting with animation and design, and working on products that feel magical and meaningful. When I’m not coding, you’ll find me exploring ideas, sipping tea, or solving LeetCode challenges. Thanks for stopping by — I hope my portfolio brings a bit of bloom and wonder to your day! 🌼
+          I enjoy building creative UIs, experimenting with animation and design, and working on products that feel magical and meaningful. When I&apos;m not coding, you&apos;ll find me exploring ideas, sipping tea, or solving LeetCode challenges. Thanks for stopping by — I hope my portfolio brings a bit of bloom and wonder to your day! 🌼
         </p>
         <Image src={'/images/Resume_pic.png'} width={100} height={100} alt="Resume Picture"
         className=" border-2 border-yellow-100 drop-shadow-[0_0_10px_#fff200] rounded-full mx-auto mt-4 transition-all"/>
