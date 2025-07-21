0568d59f27d588cd61ab395125ca926cf497bfa5
Vishalakshi-9 <vishalakshi927@gmail.com>
2025-07-22 00:52:04 +0530

    Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section id="hero" className="flex flex-col items-center justify-center text-center pt-40 pb-20 px-4 text-white relative z-10">
       <h1 className="text-5xl md:text-6xl font-playfair text-rose-100 drop-shadow-[0_0_10px_#ffd700] animate-fade-in-slow">
-        Welcome to  Vishalakshi's Code Forest
+        Welcome to  Vishalakshi&apos;s Code Forest
       </h1>
       <p className="mt-4 text-lg max-w-xl text-white/80">
         A magical realm where my code glows, creativity flows, and projects bloom. Feel free to roam here! ✨
